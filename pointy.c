0abf434fe21633c69be4e230f443ca25f27cdcb1
unknown <batmanthelegendly@gmail.com>
2018-09-15 01:56:47 -0400

    finished

--- pointy.c
+++ pointy.c
@@ -1,14 +1,33 @@
+
 #include <stdio.h>
 
 int main(){
-
+  //1
   unsigned int Foreword = 2200000001;
   char *skel = &Foreword;
-
+  //2
+  printf("\nHex \n");
   printf("%x \n", Foreword);
+
+  //3
+  printf("\nBytes \n");
   for(int c = 0; c < sizeof(Foreword); ++c){
     printf("%d: %hhx \n",c, *(skel + c));
   }
+
+  //4a
+  printf("\nIncrements \n");
+  for(int c = 0; c < sizeof(Foreword); ++c){
+    ++*(skel + c);
+  }
+  printf("%x \n",Foreword);
+  printf("%d \n", Foreword);
   
+  //4b
+  for(int c = 0; c < sizeof(Foreword); ++c){
+    *(skel + c)+=16 ;
+  }
+  printf("%x \n",Foreword);
+  printf("%d \n", Foreword);
 
 }
