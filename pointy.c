d6bb6b31288f6979064634d8e9ef26dd5a49b7db
unknown <batmanthelegendly@gmail.com>
2018-09-15 01:45:07 -0400

    1-4

--- pointy.c
+++ pointy.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main(){
+
+  unsigned int Foreword = 2200000001;
+  char *skel = &Foreword;
+
+  printf("%x \n", Foreword);
+  for(int c = 0; c < sizeof(Foreword); ++c){
+    printf("%d: %hhx \n",c, *(skel + c));
+  }
+  
+
+}
