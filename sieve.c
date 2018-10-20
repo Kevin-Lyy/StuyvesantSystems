3249ab7bc207f33050242ee1bac9eecd2671d678
unknown <batmanthelegendly@gmail.com>
2018-10-19 21:44:35 -0400

    idk

---
+++ sieve.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "sieve.h"
+
+int main(){
+  printf("TEST SIEVE\n");
+  printf("return 10 \n");
+  printf("%d\n",sieve(30));
+
+
+  return 0;
+}
--- sieve.h
+++ sieve.h
@@ -1,1 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int sieve(int nth){
+
+  if(nth <5000){nth = 1.15 * nth * log(nth);}
+  else{nth = 1.3 * nth * log(nth);}
+  char * bigboi = calloc(sizeof(*bigboi),nth);
+
+  //0 = prime
+  //1 = not prime
+  bigboi[0] = 1;
+  bigboi[1] = 1;
+
+  int c = 2;
+  for(;c<nth;c++){
+    if(bigboi[c] == 0){
+      int c2 = c+c;
+      for(;c2<nth;c2+c){
+        bigboi[c2] = 1;
+      }
+    }
+  }
+
+  int count = 0;
+  for(c = 0;c < nth;c++){
+    if(bigboi[c] == 0){
+      count++;
+    }
+  }
+  return count;
+}
