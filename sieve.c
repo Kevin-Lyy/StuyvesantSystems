d971b867f70359d10b0aa0cf3daadb54f5272247
unknown <batmanthelegendly@gmail.com>
2018-10-20 19:41:40 -0400

    done

--- sieve.c
+++ sieve.c
@@ -3,8 +3,18 @@
 
 int main(){
   printf("TEST SIEVE\n");
-  printf("return 10 \n");
+  printf("return 113 \n");
   printf("%d\n",sieve(30));
+  printf("test 1\n");
+  printf("%d\n",sieve(1));
+  printf("test 2\n");
+  printf("%d\n",sieve(2));
+  printf("test 3\n");
+  printf("%d\n",sieve(3));
+  printf("test 4\n");
+  printf("%d\n",sieve(4));
+  printf("test 5\n");
+  printf("%d\n",sieve(5));
 
 
   return 0;
--- sieve.h
+++ sieve.h
@@ -3,10 +3,14 @@
 #include <math.h>
 
 int sieve(int nth){
+  if(nth<0)return 0;
+  if(nth == 1)return 2;
 
-  if(nth <5000){nth = 1.15 * nth * log(nth);}
-  else{nth = 1.3 * nth * log(nth);}
-  char * bigboi = calloc(sizeof(*bigboi),nth);
+  int nth2;
+  if(nth <5000){nth2 = 1.15 * nth * log(nth)+10;}
+  else{nth2 = 1.3 * nth * log(nth)+10;}
+
+  char * bigboi = calloc(sizeof(bigboi),nth2);
 
   //0 = prime
   //1 = not prime
@@ -14,20 +18,30 @@ int sieve(int nth){
   bigboi[1] = 1;
 
   int c = 2;
-  for(;c<nth;c++){
+  for(;c<=nth2;c++){
     if(bigboi[c] == 0){
       int c2 = c+c;
-      for(;c2<nth;c2+c){
-        bigboi[c2] = 1;
+      for(;c2<=nth2;c2+=c){
+        bigboi[c2]=1;
+      }
+    }
+  }
+/*
+  c = 0;
+  for(;c <= nth2;c++){
+    printf("%d : %d\n",c,bigboi[c]);
+  }
+  printf("stop\n");
+*/
+  int count = 0;
+  for(c = 1;c <= nth2;c++){
+    if(bigboi[c]==0){
+      count++;
+      if(count == nth){
+        return c;
       }
     }
   }
 
-  int count = 0;
-  for(c = 0;c < nth;c++){
-    if(bigboi[c] == 0){
-      count++;
-    }
-  }
-  return count;
+//  return count;
 }
