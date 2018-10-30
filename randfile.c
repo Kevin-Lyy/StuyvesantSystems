732e3dd07bc287ecfcd0f724e4b00201f3cdd643
unknown <batmanthelegendly@gmail.com>
2018-10-29 21:58:01 -0400

    1 and 3 are 0

--- randfile.c
+++ randfile.c
@@ -7,23 +7,24 @@
 #include <unistd.h>
 
 
-int getrand(){
+int * getrand(int * randarr){
   int randata  = open("/dev/urandom", O_RDONLY );
-
-  return randata;
+  int c = 0;
+  for(;c<10;c++){
+    read(randata, &randarr[c], sizeof(int));
+  }
+  close(randata);
+  return randarr;
 }
 
 
 int main() {
+
   char* filename = "rand.txt";
-
   int * randarry = malloc(10 * sizeof(int));
+  getrand(randarry);
   int c = 0;
-  for(;c < 10; c++){
-    randarry[c] = getrand();
-  }
-
-  for(c = 0;c < 10;c++){
+  for(;c < 10;c++){
     printf("%d : %d \n",c,randarry[c]);
   }
 
