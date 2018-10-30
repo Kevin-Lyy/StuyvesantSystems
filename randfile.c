7f3d7b07a61fe8f31854b92c73ce26439aab5eb6
unknown <batmanthelegendly@gmail.com>
2018-10-29 21:40:43 -0400

    random returns -1

--- randfile.c
+++ randfile.c
@@ -7,21 +7,43 @@
 #include <unistd.h>
 
 
-int genrand(){
-  int randmata = open("/dev/urandom", O_RDONLY );
-  return randmata;
+int getrand(){
+  int randata  = open("/dev/urandom", O_RDONLY );
+
+  return randata;
 }
 
 
-int main(void) {
+int main() {
   char* filename = "rand.txt";
 
+  int * randarry = malloc(10 * sizeof(int));
+  int c = 0;
+  for(;c < 10; c++){
+    randarry[c] = getrand();
+  }
+
+  for(c = 0;c < 10;c++){
+    printf("%d : %d \n",c,randarry[c]);
+  }
+
   int file = open(filename, O_WRONLY);
-  int * to_write = malloc();
-  int writebytes = write(file,to_write,strlen(to_write));
-  free(to_write);
+  int writebytes = write(file,randarry,10 * sizeof(int));
+  printf("writing\n");
+  close(file);
+
+  file = open(filename,O_RDONLY);
+  int * readbytes = malloc(10 * sizeof(int));
+  read(file, readbytes, 10 * sizeof(int));
+  printf("reading\n");
   close(file);
 
 
+  for(c = 0;c<10;c++){
+    printf("%d %d \n",c,readbytes[c]);
+  }
+
+  free(randarry);
+  free(readbytes);
   return 0;
 }
