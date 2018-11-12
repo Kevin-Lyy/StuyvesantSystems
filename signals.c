7e60909c69f3d4050444cbd88ceac62c535a10cd
KevinLyy <batmanthelegendly@gmail.com>
2018-11-12 14:41:57 -0500

    small

--- signals.c
+++ signals.c
@@ -4,14 +4,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-#include <errno.h>
-
-void checkerrno(){
-  if(errno != 0){
-    printf("Error %d, %s\n",errno,strerror(errno));
-    exit(0);
-  }
-}
 
 void whileverboi(){
   while(666){
@@ -21,23 +13,20 @@ void whileverboi(){
 }
 
 static void signalhandler(int put){
-  checkerrno();
-  char * filename = "recieve.txt";
 
   if(put == SIGUSR1){
     printf("Parent pid %d\n",getppid());
   }
 
   if(put == SIGINT){
-    int textboi = open(filename, O_WRONLY | O_APPEND);
-    write(textboi, "Ended cause of SIGINT",strlen("Ended cause of SIGINT"));
+    int textboi = open("recieve.txt", O_WRONLY | O_APPEND);
+    write(textboi, "Ended cause of SIGINT", strlen("Ended cause of SIGINT"));
     close(textboi);
-    printf("check file for end, SIGINT \n");
+    printf("Check file for end, SIGINT \n");
     exit(0);
   }
 
 }
-
 
 int main (){
   signal(SIGINT,signalhandler);
